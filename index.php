ae4c6e56e5b797fd2d4327a657a74402f56f59d2
Nestirium <ahm.a.nest@gmail.com>
2023-07-07 02:13:58 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>VATSIM Saudi Arabia</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <meta property="og:title" content="">
+  <meta property="og:type" content="">
+  <meta property="og:url" content="">
+  <meta property="og:image" content="">
+
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+
+</head>
+
+<body>
+
+<div class="page-container">
+
+  <div class="page-header">
+    <img src="img/vACC_Logo_white_1.png" alt="The logo of VATSIM Saudi Arabia">
+    <ul>
+      <li>Policies</li>
+      <li>HQ</li>
+      <li>Our Team</li>
+      <li>Charts</li>
+      <li>Support</li>
+      <li>Login</li>
+    </ul>
+  </div>
+
+  <div class="tower-image-container">
+    <img src="img/Jeddah_New_Terminal_1_05-600x900-removebg-preview.png" alt="Jeddah ATC Tower">
+  </div>
+
+  <div class="vatmena-logo-container">
+    <img src="img/WhiteText.png" alt="VATMENA Logo">
+  </div>
+
+  <div class="vatsim-logo-container">
+    <img src="img/VATSIM_Logo_White_500px.png" alt="VATSIM Logo">
+  </div>
+
+  <div class="splash-text-container">
+    <div class="text-container">
+      <div class="title">
+        VATSIM<br>SAUDI ARABIA
+      </div>
+      <div class="subtitle">
+        The pearl of Arabia
+      </div>
+    </div>
+  </div>
+
+  <div class="aircraft-img-container">
+    <img src="img/705190-removebg-preview.png" alt="Saudi Airlines Aircraft">
+  </div>
+
+  <div class="page-footer">
+    © VATSIM Saudi Arabia - Technology Department (2023) - All rights reserved - techsupport@saudivacc.net
+  </div>
+
+</div>
+
+</body>
+
+</html>
