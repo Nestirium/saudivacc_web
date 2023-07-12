db9ed9d54e19a376134bc96eabc2789f04564b32
Nestirium <ahm.a.nest@gmail.com>
2023-07-12 16:28:42 +0300

    Implemented bootstrap.

--- index.php
+++ index.php
@@ -4,63 +4,106 @@
 <head>
   <meta charset="utf-8">
   <title>VATSIM Saudi Arabia</title>
-  <meta name="description" content="">
+  <meta name="description" content="The official VATSIM Saudi Arabia website.">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <meta property="og:title" content="">
+  <meta property="og:title" content="VATSIM Saudi Arabia">
   <meta property="og:type" content="">
   <meta property="og:url" content="">
   <meta property="og:image" content="">
 
-  <link rel="stylesheet" type="text/css" href="css/main.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
 </head>
 
 <body>
+<!-- The mother container !-->
+<div class="container-fluid bg-dark bg-gradient min-vh-100 d-flex" style="flex-direction: column;justify-content: space-between;">
 
-<div class="page-container">
-
-  <div class="page-header">
-    <img src="img/vACC_Logo_white_1.png" alt="The logo of VATSIM Saudi Arabia">
-    <ul>
-      <li>Policies</li>
-      <li>HQ</li>
-      <li>Our Team</li>
-      <li>Charts</li>
-      <li>Support</li>
-      <li>Login</li>
-    </ul>
-  </div>
-
-  <div class="tower-image-container">
-    <img src="img/Jeddah_New_Terminal_1_05-600x900-removebg-preview.png" alt="Jeddah ATC Tower">
-  </div>
-
-  <div class="vatmena-logo-container">
-    <img src="img/WhiteText.png" alt="VATMENA Logo">
-  </div>
-
-  <div class="vatsim-logo-container">
-    <img src="img/VATSIM_Logo_White_500px.png" alt="VATSIM Logo">
-  </div>
-
-  <div class="splash-text-container">
-    <div class="text-container">
-      <div class="title">
-        VATSIM<br>SAUDI ARABIA
-      </div>
-      <div class="subtitle">
-        The pearl of Arabia
-      </div>
+  <!-- First row in the grid system !-->
+  <div class="row bg-dark">
+    <!-- The row contains 1 column that spans the whole 12 units in the grid !-->
+    <div class="col-md-12">
+      <!-- The column contains a navbar using <nav> html element for SEO !-->
+      <nav class="navbar navbar-expand-md align-content-center">
+          <ul class="navbar-nav align-items-center fs-5">
+            <li class="nav-item">
+              <a class="navbar-brand nav-link" href="#">
+                <img class="img-fluid" style="width: 7%;" src="img/vACC_Logo_white_1.png" alt="VATSIM Saudi Arabia Logo">
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#">Policies</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#">HQ</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#">Staff</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#">Charts</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#">Support</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#">Login</a>
+            </li>
+          </ul>
+        </nav>
     </div>
   </div>
 
-  <div class="aircraft-img-container">
-    <img src="img/705190-removebg-preview.png" alt="Saudi Airlines Aircraft">
+  <!-- Second row in the grid system !-->
+  <div class="row mt-5">
+
+    <div class="col-md-4" style="padding-left: 0;">
+      <img class="img-fluid" alt="Jeddah ATC Tower" src="img/Jeddah_New_Terminal_1_05-600x900-removebg-preview.png">
+    </div>
+
+    <div class="col-md-2">
+
+      <div class="row">
+          <img class="img-fluid" alt="VATMENA Logo" src="img/WhiteText.png">
+      </div>
+
+      <div class="row pt-5">
+          <img class="img-fluid" alt="VATSIM Logo" src="img/VATSIM_Logo_White_500px.png">
+      </div>
+
+    </div>
+
+    <div class="col-md">
+
+      <div class="row text-white fw-lighter fs-1 justify-content-center">
+        <div class="col-md-5">
+          VATSIM<br>
+          Saudi Arabia
+        </div>
+      </div>
+
+      <div class="row text-white fw-lighter fs-2 pt-3 justify-content-center">
+        <div class="col-md-5">
+          The pearl of Arabia
+        </div>
+      </div>
+
+      <div class="row pt-5 justify-content-center">
+        <div class="col-md-9">
+          <img class="img-fluid" alt="Saudi Airlines Plane" src="img/705190-removebg-preview.png">
+        </div>
+      </div>
+
+    </div>
   </div>
 
-  <div class="page-footer">
-    © VATSIM Saudi Arabia - Technology Department (2023) - All rights reserved - techsupport@saudivacc.net
+  <!-- Third row in the grid system !-->
+  <div class="row text-white mt-5 fs-6 fw-lighter justify-content-center align-items-center flex-grow-1 bg-dark">
+    <div class="col-md-5">
+      © VATSIM Saudi Arabia Technology Department <?php echo date('Y'); ?> - All rights reserved - techsupport@saudivacc.net
+    </div>
   </div>
 
 </div>
